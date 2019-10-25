735b5007109856348bbf2bed6154dadcefc9ebbc
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-10-25 03:05:28 -0400

    Updates

---
+++ precache-manifest.808443321a0772d43649a7f60f147538.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc562a61792bf6210fe4d8247f9cb58",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c10683c0992b442772c",
+    "url": "/portfolio/static/css/main.7e83cd1e.chunk.css"
+  },
+  {
+    "revision": "b75684059916ae247179",
+    "url": "/portfolio/static/js/2.2941fd4b.chunk.js"
+  },
+  {
+    "revision": "3c10683c0992b442772c",
+    "url": "/portfolio/static/js/main.61f15111.chunk.js"
+  },
+  {
+    "revision": "d71cb2a7d8a56ffdcb2d",
+    "url": "/portfolio/static/js/runtime~main.8ec3f299.js"
+  },
+  {
+    "revision": "16b55285d79eb8f49324f24b81faf49f",
+    "url": "/portfolio/static/media/Chingu_Certificate.16b55285.jpg"
+  },
+  {
+    "revision": "01cb2c82a31c74e8f12b4e6d5f875799",
+    "url": "/portfolio/static/media/FCC_Certificate.01cb2c82.png"
+  },
+  {
+    "revision": "9685879d9b797cf28c4c7b06dcfd437f",
+    "url": "/portfolio/static/media/JobJarScreenshot.9685879d.png"
+  },
+  {
+    "revision": "b9f1cc28a701850f01527e71e2dd4ad0",
+    "url": "/portfolio/static/media/KDaceyScreenshot.b9f1cc28.png"
+  },
+  {
+    "revision": "4272162ad3a178e0de11ec3d64ee1e1d",
+    "url": "/portfolio/static/media/MarsRoverScreenshot.4272162a.png"
+  }
+]);
